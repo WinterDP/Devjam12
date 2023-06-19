cc6194f2ed3661d01c624321114735ad285c59eb
Lucas-FAlves <Lucas.3451@hotmail.com>
2023-06-18 23:45:30 -0300

    Audio Manager a funfar, assim como o menu
    FindObjectOfType<AudioManager>().PlaySound("NomeAudio");
    Usar essa linha para achar o manager e tocar o áudio

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,13 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    private void Start() 
     {
+        FindObjectOfType<AudioManager>().PlaySound("Musica Menu");
+        Debug.Log("Tocando");    
+    }
+    public void PlayGame()
+    {   
+        FindObjectOfType<AudioManager>().PlaySound("Botao");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        FindObjectOfType<AudioManager>().StopSound("Musica Menu");
     }
 
     public void QuitGame()
-    {
+    {   
+        FindObjectOfType<AudioManager>().PlaySound("Botao");
         Application.Quit();
     }
 }
